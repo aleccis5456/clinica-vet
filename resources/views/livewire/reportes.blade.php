5d67e3bfd4a424c913eaf4f1519d7efe2417088e
Alexis <alexisblugo@gmail.com>
2025-03-25 10:09:33 -0400

    se actualiza la vista de reportes para incluir un sidebar y se corrige el enlace de acceso a reportes; antes de laravel charts

--- resources/views/livewire/reportes.blade.php
+++ resources/views/livewire/reportes.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div>
+        @include('includes.sidebar-add-dueno')
+    </div>
+
+    <main class="ml-0 md:ml-64 md:pl-20 md:pt-2 pt-16 pl-2 pr-4">
+        <p class="pl-1 py-7 text-4xl font-semibold">Reportes</p>
+
+        <div class="border-t border-gray-300">
+            
+        </div>
+    </main>
 </div>
